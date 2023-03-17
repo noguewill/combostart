b1f04cc74f9703406fd9af7d3bdc71ff8b25c9b6
William Sátiro <nogue.will@gmail.com>
2023-03-17 01:10:18 -0300

    Bump

--- src/pages/Combos.jsx
+++ src/pages/Combos.jsx
@@ -5,25 +5,44 @@ function Combos() {
   return (
     <>
       <h4 className={styles.comboCard__title}>Ryu superless combo 5600</h4>
-      <section className={styles.comboCard__container}>
+      <article className={styles.comboCard__container}>
         {/* Combocard upvotes */}
         <div className={styles.comboCard_upvote__container}>
           <a>arrow</a>
           <h7>469</h7>
         </div>
+
         {/* ComboCard */}
         <div className={styles.comboCard}>
-          <section className={styles.comboCard_info__container}>
+          {/* Combo Information row */}
+          <div className={styles.comboCard_info__container}>
             <div className={styles.comboCard_charName}>RYU / master</div>
             <div className={styles.comboCard_super}>S</div>
             <div className={styles.comboCard_bars}>DRIVE</div>
             <div className={styles.comboCard_damage}>DAMAGE:5600</div>
             <div className={styles.comboCard_comboType}>Corner</div>
-          </section>
-          <section className={styles.comboCard_inputs__container}></section>
-          <section className={styles.comboCard_patch__container}></section>
+          </div>
+
+          {/* Inputs row */}
+          <div className={styles.comboCard_inputs__container}>
+            <div className={styles.input_container}>
+              <svg className={styles.input_icon}></svg>
+              <h6 className={styles.input_text}>S.MP</h6>
+            </div>
+          </div>
+
+          {/* Patch version row */}
+          <div className={styles.comboCard_patch__container}>
+            <div className={styles.patch__text}>PATCH: v1.0.0</div>
+          </div>
+          {/* Bookmark, sharing, and other options section */}
         </div>
-      </section>
+      </article>
+      <div className={styles.comboCard_social__container}>
+        <button className={styles.social_bookmark__btn}></button>
+        <button className={styles.social_share__btn}></button>
+        <button className={styles.social_more__btn}></button>
+      </div>
     </>
   );
 }
