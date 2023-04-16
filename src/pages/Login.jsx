00e262f0d8ffff776f04841f624ef604a6e3afc5
William Sátiro <nogue.will@gmail.com>
2023-04-15 21:39:50 -0300

    Login page modal almost done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,32 +1,93 @@
 import React from "react";
+import { useState } from "react";
 import styles from "@/styles/Login.module.css";
+import Image from "next/image";
 const Login = ({ toggleOverlay }) => {
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Do something with the form data
+  };
+
   return (
-    <>
-      {/* Create a classname for every single element in this page */}
-      <div className={styles.login_parent}>
-        <div className={styles.login_body}>
-          <button className={styles.close_btn} onClick={toggleOverlay}>
-            x
-          </button>
-          <h2 className={styles.signUp_btn}>
+    <div className={styles.login_parent}>
+      <div className={styles.login_body}>
+        <button className={styles.close_btn} onClick={toggleOverlay}>
+          x
+        </button>
+        <div className={styles.signUp_header_wrapper}>
+          {/*          <h2 className={styles.signUp_btn}>
             <span className={styles.signUp_logo}>COMBOSTART</span> Registration
             form
-          </h2>
-          <span>(Fields marked with an * are required)</span>
-          <form className={styles.signUp_form}>
+          </h2> */}
+          <div className={styles.signUp_logo}>
+            <Image
+              /*  className={styles.exclamation_icon} */
+              src="/logo.png"
+              alt="logo"
+              width={70}
+              height={50}
+            />
+          </div>
+          <span className={styles.signUp_notice}>
+            (Fields marked with an <span style={{ color: "red" }}>*</span> are
+            required)
+          </span>
+        </div>
+
+        {/* THE FORM */}
+        <form className={styles.signUp_form} onSubmit={handleSubmit}>
+          <div className={styles.label_wrapper}>
+            <div>
+              <span> USERNAME</span>
+              <span className={styles.required_asterisk}>*</span>
+            </div>
             <label className={styles.username_label} htmlFor="username">
-              USERNAME
               <input
                 className={styles.username_input}
                 type="text"
                 id="username"
                 name="username"
                 required
+                onChange={(e) => setPassword(e.target.value)}
               />
             </label>
+            <button className={styles.username_hoverBtn}>
+              <Image
+                style={{ left: "8.5rem" }}
+                className={styles.exclamation_icon}
+                src="/signUpExclamation.svg"
+                alt="eye"
+                width={20}
+                height={20}
+              />
+            </button>
+
+            <div className={styles.username_dialog}>
+              <p style={{ width: "12rem" }}>
+                Username must contain:
+                <br />
+                <span style={{ fontWeight: "700" }}>
+                  -From 5 to 12 characters
+                </span>
+                <br />
+                <span style={{ fontWeight: "700" }}>
+                  -Symbols not supported:
+                  <br />
+                  (e.g: "@ # $ !")
+                </span>
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.label_wrapper}>
+            <div>
+              <span> E-MAIL</span>
+              <span className={styles.required_asterisk}>*</span>
+            </div>
             <label htmlFor="email" className={styles.signUp_label}>
-              E-MAIL
               <input
                 type="email"
                 id="email"
@@ -35,43 +96,112 @@ const Login = ({ toggleOverlay }) => {
                 className={styles.email_input}
               />
             </label>
-            {/*             <label htmlFor="retype-email" className={styles.signUp_label}>
-              RE-TYPE Email
-              <input
-                type="email"
-                id="retype-email"
-                name="retype-email"
-                required
-                className={styles.email_input}
+          </div>
+
+          <div className={styles.label_wrapper}>
+            <div>
+              <span>PASSWORD</span>
+              <span className={styles.required_asterisk}>*</span>
+            </div>
+
+            {showPassword ? (
+              <>
+                <label htmlFor="password" className={styles.password_label}>
+                  <input
+                    type="text"
+                    name="password"
+                    value={password}
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                    className={styles.password_input}
+                  />
+                </label>
+              </>
+            ) : (
+              <>
+                <label htmlFor="password" className={styles.password_label}>
+                  <input
+                    type="password"
+                    name="password"
+                    value={password}
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                    className={styles.password_input}
+                  />
+                </label>
+              </>
+            )}
+
+            <button
+              className={styles.passwordEye_btn}
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              <Image
+                className={styles.exclamation_icon}
+                src={
+                  showPassword
+                    ? "/signUpPasswordEye.svg"
+                    : "/signUpPasswordClosedEye.svg"
+                }
+                alt="eye"
+                width={20}
+                height={20}
               />
-            </label> */}
-            <label htmlFor="password" className={styles.password_label}>
-              PASSWORD
-              <input
-                type="password"
-                id="password"
-                name="password"
-                required
-                className={styles.password_input}
-              />
-            </label>
-            {/*             <label htmlFor="retype-password" className={styles.password_label}>
-              RE-TYPE Password
-              <input
-                type="password"
-                id="retype-password"
-                name="retype-password"
-                required
-                className={styles.password_input}
-              />
-            </label> */}
-            <button type="submit" className={styles.submit_btn}>
-              Submit
             </button>
-          </form>
-        </div>
+            <button className={styles.password_hoverBtn}>
+              <Image
+                style={{ left: "12rem" }}
+                className={styles.exclamation_icon}
+                src="/signUpExclamation.svg"
+                alt="eye"
+                width={20}
+                height={20}
+              />
+            </button>
+            <div className={styles.dialog}>
+              <p style={{ width: "12rem" }}>
+                Password must contain:
+                <br />
+                <span style={{ fontWeight: "700" }}>
+                  -From 7 to 12 characters
+                </span>
+                <br />
+                <span style={{ fontWeight: "700" }}>-Uppercase Letter</span>
+                <br />
+                <span style={{ fontWeight: "700" }}>
+                  -Symbol:(e.g: "@ # $ !")
+                </span>
+                <br />
+                <span style={{ fontWeight: "700" }}>-Number</span>
+              </p>
+            </div>
+          </div>
+
+          <label className={styles.tos_label} htmlFor="terms-checkbox">
+            <input
+              className={styles.tos_checkbox}
+              type="checkbox"
+              id="terms-checkbox"
+            />
+            <span className={styles.tos_text}>
+              I Agree with COMBOSTART's
+              <a
+                className={styles.tos_link}
+                href="https://www.combostart.com/terms-of-service"
+              >
+                Terms of Service
+              </a>
+              <span className={styles.required_asterisk}>*</span>
+            </span>
+          </label>
+
+          <button type="submit" className={styles.submit_btn}>
+            SUBMIT
+          </button>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
