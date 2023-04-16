64ac924166cada412a554d0c7a3fff6ec6118455
William Sátiro <nogue.will@gmail.com>
2023-04-16 17:29:12 -0300

    Login options updated

---
+++ src/pages/Form.jsx
@@ -0,0 +1,187 @@
+import React from "react";
+import { useState } from "react";
+import styles from "@/styles/Form.module.css";
+import Image from "next/image";
+
+const Form = ({ toggleOverlay }) => {
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Do something with the form data
+  };
+
+  return (
+    <>
+      {/* THE FORM */}
+      <form className={styles.signUp_form} onSubmit={handleSubmit}>
+        <span className={styles.signUp_notice}>
+          (Fields marked with an <span style={{ color: "red" }}>*</span> are
+          required)
+        </span>
+        <div className={styles.label_wrapper}>
+          <div>
+            <span> USERNAME</span>
+            <span className={styles.required_asterisk}>*</span>
+          </div>
+          <label className={styles.username_label} htmlFor="username">
+            <input
+              className={styles.username_input}
+              type="text"
+              id="username"
+              name="username"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <button className={styles.username_hoverBtn}>
+            <Image
+              style={{ left: "8.5rem" }}
+              className={styles.exclamation_icon}
+              src="/signUpExclamation.svg"
+              alt="eye"
+              width={20}
+              height={20}
+            />
+          </button>
+
+          <div className={styles.username_dialog}>
+            <p style={{ width: "12rem" }}>
+              Username must contain:
+              <br />
+              <span style={{ fontWeight: "700" }}>
+                -From 5 to 12 characters
+              </span>
+              <br />
+              <span style={{ fontWeight: "700" }}>
+                -Symbols not supported:
+                <br />
+                (e.g: "@ # $ !")
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.label_wrapper}>
+          <div>
+            <span> E-MAIL</span>
+            <span className={styles.required_asterisk}>*</span>
+          </div>
+          <label htmlFor="email" className={styles.signUp_label}>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              className={styles.email_input}
+            />
+          </label>
+        </div>
+
+        <div className={styles.label_wrapper}>
+          <div>
+            <span>PASSWORD</span>
+            <span className={styles.required_asterisk}>*</span>
+          </div>
+
+          {showPassword ? (
+            <>
+              <label htmlFor="password" className={styles.password_label}>
+                <input
+                  type="text"
+                  name="password"
+                  value={password}
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                  className={styles.password_input}
+                />
+              </label>
+            </>
+          ) : (
+            <>
+              <label htmlFor="password" className={styles.password_label}>
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                  className={styles.password_input}
+                />
+              </label>
+            </>
+          )}
+
+          <button
+            className={styles.passwordEye_btn}
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            <Image
+              className={styles.exclamation_icon}
+              src={
+                showPassword
+                  ? "/signUpPasswordEye.svg"
+                  : "/signUpPasswordClosedEye.svg"
+              }
+              alt="eye"
+              width={20}
+              height={20}
+            />
+          </button>
+          <button className={styles.password_hoverBtn}>
+            <Image
+              style={{ left: "12rem" }}
+              className={styles.exclamation_icon}
+              src="/signUpExclamation.svg"
+              alt="eye"
+              width={20}
+              height={20}
+            />
+          </button>
+          <div className={styles.dialog}>
+            <p style={{ width: "12rem" }}>
+              Password must contain:
+              <br />
+              <span style={{ fontWeight: "700" }}>
+                -From 7 to 12 characters
+              </span>
+              <br />
+              <span style={{ fontWeight: "700" }}>-Uppercase Letter</span>
+              <br />
+              <span style={{ fontWeight: "700" }}>
+                -Symbol:(e.g: "@ # $ !")
+              </span>
+              <br />
+              <span style={{ fontWeight: "700" }}>-Number</span>
+            </p>
+          </div>
+        </div>
+
+        <label className={styles.tos_label} htmlFor="terms-checkbox">
+          <input
+            className={styles.tos_checkbox}
+            type="checkbox"
+            id="terms-checkbox"
+          />
+          <span className={styles.tos_text}>
+            I Agree with COMBOSTART's
+            <a
+              className={styles.tos_link}
+              href="https://www.combostart.com/terms-of-service"
+            >
+              Terms of Service
+            </a>
+            <span className={styles.required_asterisk}>*</span>
+          </span>
+        </label>
+
+        <button type="submit" className={styles.submit_btn}>
+          SUBMIT
+        </button>
+      </form>
+    </>
+  );
+};
+export default Form;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,204 +2,148 @@ import React from "react";
 import { useState } from "react";
 import styles from "@/styles/Login.module.css";
 import Image from "next/image";
+import Form from "./Form";
 const Login = ({ toggleOverlay }) => {
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Do something with the form data
-  };
-
+  const [showSignupForm, setShowSignupForm] = useState(false);
   return (
     <div className={styles.login_parent}>
       <div className={styles.login_body}>
+        {/* Button when clicked closes the modal */}
         <button className={styles.close_btn} onClick={toggleOverlay}>
           x
         </button>
+        {/* Sign Up/In Logo */}
         <div className={styles.signUp_header_wrapper}>
-          {/*          <h2 className={styles.signUp_btn}>
-            <span className={styles.signUp_logo}>COMBOSTART</span> Registration
-            form
-          </h2> */}
           <div className={styles.signUp_logo}>
             <Image
-              /*  className={styles.exclamation_icon} */
               src="/logo.png"
-              alt="logo"
+              alt="combostart logo"
               width={70}
               height={50}
             />
           </div>
-          <span className={styles.signUp_notice}>
-            (Fields marked with an <span style={{ color: "red" }}>*</span> are
-            required)
-          </span>
         </div>
 
-        {/* THE FORM */}
-        <form className={styles.signUp_form} onSubmit={handleSubmit}>
-          <div className={styles.label_wrapper}>
-            <div>
-              <span> USERNAME</span>
-              <span className={styles.required_asterisk}>*</span>
-            </div>
-            <label className={styles.username_label} htmlFor="username">
-              <input
-                className={styles.username_input}
-                type="text"
-                id="username"
-                name="username"
-                required
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </label>
-            <button className={styles.username_hoverBtn}>
-              <Image
-                style={{ left: "8.5rem" }}
-                className={styles.exclamation_icon}
-                src="/signUpExclamation.svg"
-                alt="eye"
-                width={20}
-                height={20}
-              />
-            </button>
-
-            <div className={styles.username_dialog}>
-              <p style={{ width: "12rem" }}>
-                Username must contain:
-                <br />
-                <span style={{ fontWeight: "700" }}>
-                  -From 5 to 12 characters
-                </span>
-                <br />
-                <span style={{ fontWeight: "700" }}>
-                  -Symbols not supported:
-                  <br />
-                  (e.g: "@ # $ !")
-                </span>
-              </p>
-            </div>
-          </div>
-
-          <div className={styles.label_wrapper}>
-            <div>
-              <span> E-MAIL</span>
-              <span className={styles.required_asterisk}>*</span>
-            </div>
-            <label htmlFor="email" className={styles.signUp_label}>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                required
-                className={styles.email_input}
-              />
-            </label>
-          </div>
-
-          <div className={styles.label_wrapper}>
-            <div>
-              <span>PASSWORD</span>
-              <span className={styles.required_asterisk}>*</span>
-            </div>
-
-            {showPassword ? (
-              <>
-                <label htmlFor="password" className={styles.password_label}>
-                  <input
-                    type="text"
-                    name="password"
-                    value={password}
-                    required
-                    onChange={(e) => setPassword(e.target.value)}
-                    className={styles.password_input}
-                  />
-                </label>
-              </>
-            ) : (
-              <>
-                <label htmlFor="password" className={styles.password_label}>
-                  <input
-                    type="password"
-                    name="password"
-                    value={password}
-                    required
-                    onChange={(e) => setPassword(e.target.value)}
-                    className={styles.password_input}
-                  />
-                </label>
-              </>
-            )}
-
-            <button
-              className={styles.passwordEye_btn}
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              <Image
-                className={styles.exclamation_icon}
-                src={
-                  showPassword
-                    ? "/signUpPasswordEye.svg"
-                    : "/signUpPasswordClosedEye.svg"
-                }
-                alt="eye"
-                width={20}
-                height={20}
-              />
-            </button>
-            <button className={styles.password_hoverBtn}>
-              <Image
-                style={{ left: "12rem" }}
-                className={styles.exclamation_icon}
-                src="/signUpExclamation.svg"
-                alt="eye"
-                width={20}
-                height={20}
-              />
-            </button>
-            <div className={styles.dialog}>
-              <p style={{ width: "12rem" }}>
-                Password must contain:
-                <br />
-                <span style={{ fontWeight: "700" }}>
-                  -From 7 to 12 characters
-                </span>
-                <br />
-                <span style={{ fontWeight: "700" }}>-Uppercase Letter</span>
-                <br />
-                <span style={{ fontWeight: "700" }}>
-                  -Symbol:(e.g: "@ # $ !")
-                </span>
-                <br />
-                <span style={{ fontWeight: "700" }}>-Number</span>
-              </p>
-            </div>
-          </div>
-
-          <label className={styles.tos_label} htmlFor="terms-checkbox">
-            <input
-              className={styles.tos_checkbox}
-              type="checkbox"
-              id="terms-checkbox"
-            />
-            <span className={styles.tos_text}>
-              I Agree with COMBOSTART's
-              <a
-                className={styles.tos_link}
-                href="https://www.combostart.com/terms-of-service"
+        {/* Sign Up Form */}
+        {showSignupForm ? (
+          <Form toggleOverlay={toggleOverlay} />
+        ) : (
+          <>
+            <h3 className={styles.signUp_message}>Sign up and join us!</h3>
+            <div className={styles.signOption_wrapper}>
+              {/* Sign Up with your E-mail */}
+              <button
+                className={styles.signOption_btn}
+                onClick={() => setShowSignupForm(true)}
               >
-                Terms of Service
-              </a>
-              <span className={styles.required_asterisk}>*</span>
-            </span>
-          </label>
-
-          <button type="submit" className={styles.submit_btn}>
-            SUBMIT
-          </button>
-        </form>
+                <span>Sign Up with E-mail</span>
+                <div className={styles.signOptionIcon}>
+                  <Image
+                    src="/logo.png"
+                    alt="combostart logo"
+                    width={26}
+                    height={26}
+                  />
+                </div>
+              </button>
+              {/* Sign Up using your Google account */}
+              <button className={styles.signOption_btn}>
+                <span>Sign Up with Google</span>
+                <div
+                  className={styles.signOptionIcon}
+                  style={{ backgroundColor: "white" }}
+                >
+                  <Image
+                    src="/googleIcon.svg"
+                    alt="Google logo"
+                    width={26}
+                    height={26}
+                  />
+                </div>
+              </button>
+              {/* Sign up using your Steam account */}
+              <button className={styles.signOption_btn}>
+                <span>Sign Up with Steam</span>
+                <div
+                  className={styles.signOptionIcon}
+                  style={{ backgroundColor: "rgb(43, 108, 154)  " }}
+                >
+                  {" "}
+                  <Image
+                    src="/steamIcon.svg"
+                    alt="Steam logo"
+                    width={26}
+                    height={26}
+                  />
+                </div>
+              </button>
+              {/* Sign up using your Discord account */}
+              <button className={styles.signOption_btn}>
+                <span>Sign Up with Discord</span>
+                <div
+                  className={styles.signOptionIcon}
+                  style={{ backgroundColor: "#7289DA" }}
+                >
+                  {" "}
+                  <Image
+                    src="/discordIcon.svg"
+                    alt="Discord logo"
+                    width={26}
+                    height={26}
+                  />
+                </div>
+              </button>
+              {/* Sign up using your Xbox account */}
+              <button className={styles.signOption_btn}>
+                <span>Sign Up with Xbox Live</span>
+                <div
+                  className={styles.signOptionIcon}
+                  style={{ backgroundColor: "green" }}
+                >
+                  {" "}
+                  <Image
+                    src="/xboxIcon.svg"
+                    alt="Xbox logo"
+                    width={26}
+                    height={26}
+                  />
+                </div>
+              </button>
+              {/* Sign up using your Playstation account */}
+              <button className={styles.signOption_btn}>
+                <span>Sign Up with Playstation Network</span>
+                <div
+                  className={styles.signOptionIcon}
+                  style={{ backgroundColor: "#003087 " }}
+                >
+                  {" "}
+                  <Image
+                    src="/playstationIcon.svg"
+                    alt="Playstation logo"
+                    width={26}
+                    height={26}
+                  />
+                </div>
+              </button>
+              <span>OR</span>
+            </div>
+            {/* Sign In Notice */}
+          </>
+        )}
+        <span className={styles.signIn_text}>
+          {showSignupForm ? (
+            <>
+              {" "}
+              <span>Go back to the other sign up options</span> <br />{" "}
+              <span>Already have an account?</span>
+            </>
+          ) : (
+            <span>Already have an account?</span>
+          )}
+          <a href="/" className={styles.signIn_link}>
+            Sign in
+          </a>
+        </span>
       </div>
     </div>
   );
