c56378c0cc4c366bade02b7b41186ad881e6c86c
William Sátiro <nogue.will@gmail.com>
2023-04-19 04:17:07 -0300

    ComboHub several logic and visual adds

--- src/pages/ComboHub.jsx
+++ src/pages/ComboHub.jsx
@@ -2,10 +2,17 @@ import React, { useState } from "react";
 import styles from "@/styles/ComboHub.module.css";
 import Navbar from "./Navbar";
 import Link from "next/link";
+import Search from "./Search";
 
 const ComboHub = () => {
   const [cardLimit, setCardLimit] = useState(10);
+  const [dataFromChild, setDataFromChild] = useState("");
+  const colorStyle =
+    dataFromChild === 1 ? { backgroundColor: "" } : { backgroundColor: "" };
 
+  const handleDataFromChild = (data) => {
+    setDataFromChild(data);
+  };
   const cards = [...Array(cardLimit * 3)].map((_, i) => (
     <Link key={i} href="#" className={styles.card}>
       <h2 className={styles.cardTitle}>Card {i + 1}</h2>
@@ -26,11 +33,33 @@ const ComboHub = () => {
   return (
     <div className={styles.bigboi}>
       <Navbar btnType={`classic`} />
+      <Search btnType={`classic`} onData={handleDataFromChild} />
       <div className={styles.container}>
         <div className={styles.topRow}>
-          <div className={styles.square1}>1</div>
-          <div className={styles.square2}>2</div>
-          <div className={styles.square3}>3</div>
+          <Link
+            href={"/"}
+            className={styles.featuredCard_left}
+            style={colorStyle}
+          >
+            <span className={styles.cardTitle}>STREET FIGHTER 6</span>
+            <div className={styles.square1}></div>
+          </Link>
+          <Link
+            href={"/"}
+            className={styles.featuredCard_mid}
+            style={colorStyle}
+          >
+            <span className={styles.cardTitle}>TEKKEN 7</span>
+            <div className={styles.square2}></div>
+          </Link>
+          <Link
+            href={"/"}
+            className={styles.featuredCard_right}
+            style={colorStyle}
+          >
+            <span className={styles.cardTitle}>MORTAL KOMBAT 11</span>
+            <div className={styles.square3}></div>
+          </Link>
         </div>
         <div className={styles.bottomRow}>{cards.slice(0, cardLimit * 3)}</div>
         {cardLimit < 10 && (
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "@/styles/Search.module.css";
 import Dropdown from "./Dropdown";
+import { useRouter } from "next/router";
 
-function Search() {
+const Search = ({ btnType, themeOverride, onData }) => {
+  const componentName = "/" + Search.name;
+  const router = useRouter();
+  const pathname = router.pathname;
+  const [subDirectory, setSubDirectory] = useState(componentName);
+  const [activeButton, setActiveButton] = useState(1);
+
   function handleOptionSelect(option) {
     console.log(`Selected option: ${option}`);
   }
+
+  function handleButtonClick(buttonIndex) {
+    setActiveButton(buttonIndex);
+  }
+  onData(activeButton);
 
   return (
     /* Mobile Search Section */
@@ -18,46 +30,95 @@ function Search() {
       <div className={styles.search_main__container}>
         <div className={styles.search_container__child}>
           {/* Mobile Combos Header Text */}
-          <h1 className={styles.search__comboSection_header}>COMBO GUIDES</h1>
+          <h1 className={styles[`${btnType}__comboSection_header`]}>
+            COMBO GUIDES
+          </h1>
 
-          {/* Mobile Filter Options */}
-          <div className={styles.search__filterContainer}>
-            {/* Sort filter option */}
-            <div className={styles.search__filterOption_container}>
-              <h6 className={styles.search__filterBtnText}> SORT </h6>
-              {/* <button className={styles.search__filterBtn}>TOP RATED</button> */}
-              <Dropdown
-                options={["Top Rated", "Popular", "Reviewed"]}
-                defaultOption="Trending"
-                onOptionSelect={handleOptionSelect}
-              />
+          {pathname === subDirectory ? (
+            <div className={styles.search__filterContainer}>
+              {/* Sort filter option */}
+              <div className={styles.search__filterOption_container}>
+                <h6 className={styles.search__filterBtnText}> SORT </h6>
+
+                <Dropdown
+                  options={["Top Rated", "Popular", "Reviewed"]}
+                  defaultOption="Trending"
+                  onOptionSelect={handleOptionSelect}
+                />
+              </div>
+              {/* Author filter option */}
+              <div className={styles.search__filterOption_container}>
+                <h6 className={styles.search__filterBtnText}> AUTHOR </h6>
+
+                <Dropdown
+                  options={["Creator", "Popular", "Pro"]}
+                  defaultOption="Points"
+                  onOptionSelect={handleOptionSelect}
+                />
+              </div>
+              {/* Character filter option */}
+              <div className={styles.search__filterOption_container}>
+                <h6 className={styles.search__filterBtnText}> CHARACTER</h6>
+
+                <Dropdown
+                  options={["Ryu", "Ken", "Juri"]}
+                  defaultOption="none"
+                  onOptionSelect={handleOptionSelect}
+                />
+              </div>
             </div>
-            {/* Author filter option */}
-            <div className={styles.search__filterOption_container}>
-              <h6 className={styles.search__filterBtnText}> AUTHOR </h6>
-              {/* <button className={styles.search__filterBtn}>RANKED</button> */}
-              <Dropdown
-                options={["Creator", "Popular", "Pro"]}
-                defaultOption="Points"
-                onOptionSelect={handleOptionSelect}
-              />
+          ) : (
+            <div className={styles.classic_filter_container}>
+              <span className={styles.classic_filter_label}>SORT:</span>
+              <button
+                className={
+                  styles[
+                    `${
+                      activeButton === 1
+                        ? "classic_filter_container_btn--active"
+                        : "classic_filter_container_btn"
+                    }`
+                  ]
+                }
+                onClick={() => handleButtonClick(1)}
+              >
+                Popular
+              </button>
+              <button
+                className={
+                  styles[
+                    `${
+                      activeButton === 2
+                        ? "classic_filter_container_btn--active"
+                        : "classic_filter_container_btn"
+                    }`
+                  ]
+                }
+                onClick={() => handleButtonClick(2)}
+              >
+                Curated for you
+              </button>
+              <button
+                className={
+                  styles[
+                    `${
+                      activeButton === 3
+                        ? "classic_filter_container_btn--active"
+                        : "classic_filter_container_btn"
+                    }`
+                  ]
+                }
+                onClick={() => handleButtonClick(3)}
+              >
+                Following
+              </button>
             </div>
-            {/* Character filter option */}
-            <div className={styles.search__filterOption_container}>
-              <h6 className={styles.search__filterBtnText}> CHARACTER</h6>
-              {/* <button className={styles.search__filterBtn}>NONE</button> */}
-              <Dropdown
-                options={["Ryu", "Ken", "Juri"]}
-                defaultOption="none"
-                onOptionSelect={handleOptionSelect}
-              />
-            </div>
-          </div>
+          )}
 
           {/* Mobile Searchbar */}
-          <label className={styles.searchBar_container}>
+          <label className={styles[`${btnType}__searchBar_container`]}>
             <input
-              className={styles.searchBar_input}
+              className={styles[`${btnType}__searchBar_input`]}
               type="text"
               placeholder="Ryu shoryuken superless combo"
             />
@@ -66,5 +127,5 @@ function Search() {
       </div>
     </div>
   );
-}
+};
 export default Search;
