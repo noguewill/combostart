9f135bd3c013f5685638d5732fe3a7f85cdf6d73
William Sátiro <nogue.will@gmail.com>
2023-06-11 08:41:17 -0300

    "Enviroment variable set and hooked"

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,8 +5,8 @@ import ComboHub from "./ComboHub";
 import Cookies from "js-cookie";
 import { useState, useEffect } from "react";
 import { Amplify } from "aws-amplify";
-/* import awsconfig from "src/aws-exports.js";
-Amplify.configure(awsconfig); */
+/* import awsconfig from "src/aws-exports.js"; */
+/* Amplify.configure(awsconfig); */
 
 export default function Home() {
   const [showWelcome, setShowWelcome] = useState(false);
@@ -29,6 +29,28 @@ export default function Home() {
       Cookies.set("visited", "true", { expires: 7 });
       setShowWelcome(true);
     }
+  }, []);
+
+  useEffect(() => {
+    // Set the necessary AWS Amplify configuration
+    Amplify.configure({
+      aws_project_region: process.env.AWS_PROJECT_REGION,
+      aws_cognito_identity_pool_id: process.env.AWS_COGNITO_IDENTITY_POOL_ID,
+      aws_cognito_region: process.env.AWS_COGNITO_REGION,
+      aws_user_pools_id: process.env.AWS_USER_POOLS_ID,
+      aws_user_pools_web_client_id: process.env.AWS_USER_POOLS_WEB_CLIENT_ID,
+      oauth: {},
+      aws_cognito_username_attributes: [],
+      aws_cognito_social_providers: [],
+      aws_cognito_signup_attributes: ["EMAIL"],
+      aws_cognito_mfa_configuration: "OFF",
+      aws_cognito_mfa_types: ["SMS"],
+      aws_cognito_password_protection_settings: {
+        passwordPolicyMinLength: 8,
+        passwordPolicyCharacters: [],
+      },
+      aws_cognito_verification_mechanisms: ["EMAIL"],
+    });
   }, []);
 
   return (
