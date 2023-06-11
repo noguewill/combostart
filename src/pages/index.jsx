5f1a5a9a5c041032b3b56fbd4dd1898f4ac2a2d7
William Sátiro <nogue.will@gmail.com>
2023-06-11 08:11:29 -0300

    'bump'

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,8 +5,8 @@ import ComboHub from "./ComboHub";
 import Cookies from "js-cookie";
 import { useState, useEffect } from "react";
 import { Amplify } from "aws-amplify";
-import awsconfig from "C:/Users/nogue/Documents/Create/sfc/sfcombos/src/aws-exports.js";
-Amplify.configure(awsconfig);
+/* import awsconfig from "src/aws-exports.js";
+Amplify.configure(awsconfig); */
 
 export default function Home() {
   const [showWelcome, setShowWelcome] = useState(false);
