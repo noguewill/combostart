2b0387573a3c7f0c9cb909706222cd39b10d7c04
William Sátiro <nogue.will@gmail.com>
2023-06-16 07:35:11 -0300

    'bump'

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const fs = require("fs");
-
-const dotenv = require("dotenv");
-
-/* const env = dotenv.parse(fs.readFileSync(".env")); */
-
 const nextConfig = {
   reactStrictMode: true,
-
-  env: env,
 };
 
 module.exports = nextConfig;
