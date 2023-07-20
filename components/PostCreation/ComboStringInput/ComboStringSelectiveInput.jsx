ed387e577dde85f99ef90a9c52355244461cd776
William Sátiro <nogue.will@gmail.com>
2023-07-20 17:04:05 -0300

    Input image names, comboStringselectiveInput logic

--- components/PostCreation/ComboStringInput/ComboStringSelectiveInput.jsx
+++ components/PostCreation/ComboStringInput/ComboStringSelectiveInput.jsx
@@ -1,9 +1,87 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import styles from "@/styles/NewPost_inputs_styles/ComboString_styles/ComboStringSelectiveInput.module.css";
 import Image from "next/image";
 import DirectionalInputs from "./DirectionalInputs";
 
 const ComboStringSelectiveInput = ({ theme }) => {
+  const [inputValue, setInputValue] = useState([]);
+  const [comboString, setComboString] = useState([]);
+  const [isPlusOnly, setIsPlusOnly] = useState(false);
+
+  const handleInputValue = (val) => {
+    if (val === "+") {
+      if (inputValue.length === 0) {
+        setIsPlusOnly(true); // "+" is added alone, disable "ADD STRING" button
+      } else {
+        setIsPlusOnly(false); // Reset the isPlusOnly state when a non-"+" value is added
+        setInputValue((prevInputValue) => [
+          ...prevInputValue,
+          { type: "text", value: val },
+        ]);
+      }
+    } else {
+      setIsPlusOnly(false); // Reset the isPlusOnly state when a non-"+" value is added
+      setInputValue((prevInputValue) => [
+        ...prevInputValue,
+        { type: "image", value: val },
+      ]);
+    }
+    // Prevent form submission
+    event.preventDefault();
+  };
+
+  const handleAddString = useCallback(() => {
+    const isPlusOnlyValue =
+      inputValue.length === 1 && inputValue[0].value === "+";
+
+    if (inputValue.length > 0 && !isPlusOnlyValue) {
+      const newComboString = inputValue.map((item) => item.value).join(", ");
+      setComboString((prevComboString) => [...prevComboString, inputValue]);
+      setInputValue([]);
+    }
+    // Prevent form submission
+    event.preventDefault();
+  }, [inputValue]);
+
+  const handleRemoveComboString = (index) => {
+    setComboString((prevComboString) => {
+      const updatedComboString = [...prevComboString];
+      updatedComboString.splice(index, 1);
+      return updatedComboString;
+    });
+    // Prevent form submission
+    event.preventDefault();
+  };
+
+  useEffect(() => {
+    // Check if the inputValue contains only the "+" sign
+    const isPlusOnlyValue =
+      inputValue.length === 1 && inputValue[0].value === "+";
+    setIsPlusOnly(isPlusOnlyValue);
+  }, [inputValue]);
+
+  const handleRemoveInputValue = (index) => {
+    setInputValue((prevInputValue) => {
+      const updatedInputValue = [...prevInputValue];
+      updatedInputValue.splice(index, 1);
+      return updatedInputValue;
+    });
+
+    event.preventDefault();
+  };
+
+  const handleResetStrings = () => {
+    const confirmReset = window.confirm(
+      "Are you sure you want to reset all combo strings?"
+    );
+    if (confirmReset) {
+      setInputValue([]);
+      setComboString([]);
+    }
+    // Prevent form submission
+    event.preventDefault();
+  };
+
   return (
     <div className={styles[`${theme}container`]}>
       <div className={styles.header_container}>
@@ -13,139 +91,292 @@ const ComboStringSelectiveInput = ({ theme }) => {
         </h2>
       </div>
       <main className={styles[`${theme}comboBtns_container`]}>
-        <DirectionalInputs theme={theme} />
-        <button className={styles.addInput_btn}>+</button>
+        <DirectionalInputs theme={theme} handleInputValue={handleInputValue} />
+
+        <button
+          className={styles.addInput_btn}
+          onClick={() => handleInputValue("+")}
+        >
+          +
+        </button>
 
         <div className={styles.buttonInputs_row_container}>
           <div className={styles.buttonInputs_row}>
             <div className={styles.buttonInput_container}>
-              <Image
-                src="/inputs/punchA.svg"
-                width={32}
-                height={32}
+              <button
                 className={styles.buttonInput}
-              />
-              <span>Punch</span>
+                onClick={() => handleInputValue("P")}
+              >
+                <Image
+                  src="/inputs/P.svg"
+                  width={32}
+                  height={32}
+                  alt="Any punch"
+                />
+              </button>
+              <span className={styles.buttonInput_text}>ANY PUNCH</span>
             </div>
             <div className={styles.buttonInput_container}>
-              <Image
-                src="/inputs/punchA.svg"
-                width={32}
-                height={32}
+              <button
                 className={styles.buttonInput}
-              />
-              <span>Punch</span>
+                onClick={() => handleInputValue("LP")}
+              >
+                <Image
+                  src="/inputs/LP.svg"
+                  width={32}
+                  height={32}
+                  alt="Light Punch"
+                />
+              </button>
+              <span className={styles.buttonInput_text}>LIGHT PUNCH</span>
             </div>
             <div className={styles.buttonInput_container}>
-              <Image
-                src="/inputs/punchA.svg"
-                width={32}
-                height={32}
+              <button
                 className={styles.buttonInput}
-              />
-              <span>Punch</span>
+                onClick={() => handleInputValue("MP")}
+              >
+                <Image
+                  src="/inputs/MP.svg"
+                  width={32}
+                  height={32}
+                  alt="Medium Punch"
+                />
+              </button>
+              <span className={styles.buttonInput_text}>MEDIUM PUNCH</span>
             </div>
             <div className={styles.buttonInput_container}>
-              <Image
-                src="/inputs/punchA.svg"
-                width={32}
-                height={32}
+              <button
                 className={styles.buttonInput}
-              />
-              <span>Punch</span>
+                onClick={() => handleInputValue("HP")}
+              >
+                <Image
+                  src="/inputs/HP.svg"
+                  width={32}
+                  height={32}
+                  alt="Heavy Punch"
+                />
+              </button>
+              <span className={styles.buttonInput_text}>HEAVY PUNCH</span>
             </div>
             <div className={styles.buttonInput_container}>
-              <Image
-                src="/inputs/punchA.svg"
-                width={32}
-                height={32}
+              <button
                 className={styles.buttonInput}
-              />
-              <span>Punch</span>
+                onClick={() => handleInputValue("oPunch")}
+              >
+                <Image
+                  src="/inputs/Punch.svg"
+                  width={32}
+                  height={32}
+                  alt="Overdrive Punch"
+                />
+              </button>
+              <span className={styles.buttonInput_text}>OVERDRIVE PUNCH</span>
             </div>
           </div>
           <div className={styles.buttonInputs_row}>
             <div className={styles.buttonInput_container}>
-              <Image
-                src="/inputs/punchA.svg"
-                width={32}
-                height={32}
+              <button
                 className={styles.buttonInput}
-              />
-              <span>Punch</span>
+                onClick={() => handleInputValue("K")}
+              >
+                <Image
+                  src="/inputs/K.svg"
+                  width={32}
+                  height={32}
+                  alt="Any kick"
+                />
+              </button>
+              <span className={styles.buttonInput_text}>ANY KICK</span>
             </div>
             <div className={styles.buttonInput_container}>
-              <Image
-                src="/inputs/punchA.svg"
-                width={32}
-                height={32}
+              <button
                 className={styles.buttonInput}
-              />
-              <span>Punch</span>
+                onClick={() => handleInputValue("LK")}
+              >
+                <Image
+                  src="/inputs/LK.svg"
+                  width={32}
+                  height={32}
+                  alt="Light Kick"
+                />
+              </button>
+              <span className={styles.buttonInput_text}>LIGHT KICK</span>
             </div>
             <div className={styles.buttonInput_container}>
-              <Image
-                src="/inputs/punchA.svg"
-                width={32}
-                height={32}
+              <button
                 className={styles.buttonInput}
-              />
-              <span>Punch</span>
+                onClick={() => handleInputValue("MK")}
+              >
+                <Image
+                  src="/inputs/MK.svg"
+                  width={32}
+                  height={32}
+                  alt="Medium Kick"
+                />
+              </button>
+              <span className={styles.buttonInput_text}>MEDIUM KICK</span>
             </div>
             <div className={styles.buttonInput_container}>
-              <Image
-                src="/inputs/punchA.svg"
-                width={32}
-                height={32}
+              <button
                 className={styles.buttonInput}
-              />
-              <span>Punch</span>
+                onClick={() => handleInputValue("HK")}
+              >
+                <Image
+                  src="/inputs/HK.svg"
+                  width={32}
+                  height={32}
+                  alt="Heavy Kick"
+                />
+              </button>
+              <span className={styles.buttonInput_text}>HEAVY KICK</span>
             </div>
             <div className={styles.buttonInput_container}>
-              <Image
-                src="/inputs/punchA.svg"
-                width={32}
-                height={32}
+              <button
                 className={styles.buttonInput}
-              />
-              <span>Punch</span>
+                onClick={() => handleInputValue("okick")}
+              >
+                <Image
+                  src="/inputs/Kick.svg"
+                  width={32}
+                  height={32}
+                  alt="Overdrive Kick"
+                />
+              </button>
+              <span className={styles.buttonInput_text}>OVERDRIVE KICK</span>
             </div>
           </div>
         </div>
 
         <div className={styles.mechanicsColumn_container}>
           <div className={styles.buttonInput_container}>
+            <button
+              className={styles.buttonInput}
+              onClick={() => handleInputValue("cancel")}
+            >
+              <Image src="/inputs/P.svg" width={32} height={32} alt="cancel" />
+            </button>
+            <span className={styles.buttonInput_text}>CANCEL ( xx )</span>
+          </div>
+          <div className={styles.buttonInput_container}>
             <Image
-              src="/inputs/punchA.svg"
+              src="/inputs/PC.svg"
               width={32}
               height={32}
               className={styles.buttonInput}
+              alt="Yeah"
             />
             <span>Punch</span>
           </div>
           <div className={styles.buttonInput_container}>
             <Image
-              src="/inputs/punchA.svg"
+              src="/inputs/DR.svg"
               width={32}
               height={32}
               className={styles.buttonInput}
-            />
-            <span>Punch</span>
-          </div>
-          <div className={styles.buttonInput_container}>
-            <Image
-              src="/inputs/punchA.svg"
-              width={32}
-              height={32}
-              className={styles.buttonInput}
+              alt="Yeah"
             />
             <span>Punch</span>
           </div>
         </div>
       </main>
-      <button className={styles.addString_btn}>ADD STRING</button>
-      <div className={styles.comboStrings_container}>{/* Mappping of the inputs that user clicked */}</div>
-      <button className={styles.saveStrings_btn}>SAVE COMBO STRINGS</button>
+      <div className={styles.stringHandleBtn_container}>
+        {!isPlusOnly && inputValue.length > 0 && (
+          <button
+            className={styles.addString_btn}
+            onClick={(e) => {
+              e.preventDefault();
+              handleAddString();
+            }}
+          >
+            ADD STRING
+          </button>
+        )}
+
+        {comboString.length > 0 && (
+          <button
+            className={styles.resetStrings_btn}
+            onClick={(e) => {
+              e.preventDefault();
+              handleResetStrings();
+            }}
+          >
+            RESET STRINGS
+          </button>
+        )}
+      </div>
+      {(inputValue.length > 0 || comboString.length > 0) && (
+        <div className={styles.comboStrings_container}>
+          {comboString.map((subArray, outerIndex) => (
+            <div className={styles.comboString_wrapper}>
+              <input
+                className={styles.comboString_input}
+                type="text"
+                placeholder="FULLY CHARGED"
+              />
+              <div
+                key={outerIndex}
+                className={styles.comboStringRow}
+                onClick={() => handleRemoveComboString(outerIndex)}
+              >
+                {subArray.map((item, innerIndex) => (
+                  <div key={innerIndex} className={styles.comboString}>
+                    {item.type === "text" ? (
+                      item.value
+                    ) : (
+                      <div className={styles.comboString_string_container}>
+                        <Image
+                          src={`/inputs/${item.value}.svg`}
+                          width={32}
+                          height={32}
+                          alt={item.value}
+                        />
+                        <span className={styles.comboString_text}>
+                          {item.value}
+                        </span>
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+
+          {inputValue.map((item, index) => (
+            <div key={index} className={styles.comboString}>
+              {item.type === "text" ? (
+                <div className={styles.comboString_string_container}>
+                  <span>{item.value}</span>
+                  <button
+                    className={styles.removeInput_btn}
+                    onClick={() => handleRemoveInputValue(index)}
+                  >
+                    REMOVE
+                  </button>
+                </div>
+              ) : (
+                <div className={styles.comboString_string_container}>
+                  <Image
+                    src={`/inputs/${item.value}.svg`}
+                    width={32}
+                    height={32}
+                    alt={item.value}
+                  />
+                  <button
+                    className={styles.removeInput_btn}
+                    onClick={() => handleRemoveInputValue(index)}
+                  >
+                    REMOVE
+                  </button>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
+
+      {(inputValue.length > 0 || comboString.length > 0) && (
+        <button className={styles.saveStrings_btn}>SAVE COMBO STRINGS</button>
+      )}
     </div>
   );
 };
--- components/PostCreation/ComboStringInput/DirectionalInputs.jsx
+++ components/PostCreation/ComboStringInput/DirectionalInputs.jsx
@@ -1,82 +1,114 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "@/styles/NewPost_inputs_styles/ComboString_styles/Directional_inputs.module.css";
 import Image from "next/image";
 
-const DirectionalInputs = ({ theme }) => {
+const DirectionalInputs = ({ theme, handleInputValue }) => {
   return (
     <div className={styles.directionals_section}>
       <div className={styles.directionals_container}>
         <div className={styles.directionals_column}>
           <Image
+            id="upback"
             className={styles.directional_btn}
             src="/inputs/arrow_up.svg"
             width={42}
             height={42}
+            onClick={handleInputValue}
           />
           <Image
+            id="back"
             className={styles.directional_btn}
             src="/inputs/arrow_up.svg"
             width={42}
             height={42}
+            onClick={handleInputValue}
           />
           <Image
+            id="downback"
             className={styles.directional_btn}
             src="/inputs/arrow_up.svg"
             width={42}
             height={42}
+            onClick={handleInputValue}
           />
         </div>
         <div className={styles.directionals_column}>
           <Image
+            id="up"
             className={styles.directional_btn}
             src="/inputs/arrow_up.svg"
             width={42}
             height={42}
+            onClick={handleInputValue}
           />
           <span className={styles.neutalDot}></span>
           <Image
+            id="down"
             className={styles.directional_btn}
             src="/inputs/arrow_up.svg"
             width={42}
             height={42}
+            onClick={handleInputValue}
           />
         </div>
         <div className={styles.directionals_column}>
           <Image
+            id="upforward"
             className={styles.directional_btn}
             src="/inputs/arrow_up.svg"
             width={42}
             height={42}
+            onClick={handleInputValue}
           />
           <Image
+            id="forward"
             className={styles.directional_btn}
             src="/inputs/arrow_up.svg"
             width={42}
             height={42}
+            onClick={handleInputValue}
           />
           <Image
+            id="downforward"
             className={styles.directional_btn}
             src="/inputs/arrow_up.svg"
             width={42}
             height={42}
+            onClick={handleInputValue}
           />
         </div>
       </div>
 
       <div className={styles.directionals_acronyms_container}>
         <div className={styles.directionals_aconyms_row}>
-          <button className={styles[`${theme}directional_acronym_btn`]}>
+          <button
+            id="QCF"
+            className={styles[`${theme}directional_acronym_btn`]}
+            onClick={handleInputValue}
+          >
             QCF
           </button>
-          <button className={styles[`${theme}directional_acronym_btn`]}>
+          <button
+            id="QCB"
+            className={styles[`${theme}directional_acronym_btn`]}
+            onClick={handleInputValue}
+          >
             QCB
           </button>
         </div>
         <div className={styles.directionals_aconyms_row}>
-          <button className={styles[`${theme}directional_acronym_btn`]}>
+          <button
+            id="HCF"
+            className={styles[`${theme}directional_acronym_btn`]}
+            onClick={handleInputValue}
+          >
             HCF
           </button>
-          <button className={styles[`${theme}directional_acronym_btn`]}>
+          <button
+            id="HCB"
+            className={styles[`${theme}directional_acronym_btn`]}
+            onClick={handleInputValue}
+          >
             HCB
           </button>
         </div>
