e959587bc77ed8327e85ec527779787c7e8c35aa
William Sátiro <nogue.will@gmail.com>
2023-09-19 11:08:12 -0300

    GA bump

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,21 +6,19 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Script
+        async
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         id="google-analytics-script"
+        defer
       />
 
       <Script strategy="lazyOnload" id="google-analytics-inline-script">
-        {" "}
-        {/* Add the id attribute */}
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname,
-          });
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
         `}
       </Script>
       <ThemeProvider>
